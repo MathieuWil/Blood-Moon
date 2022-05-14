76f3506dd7cb3e9648bb629daa2b5be1f3c7b07c
MathieuWil <mathieu0108w@gmail.com>
2022-05-15 00:26:10 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C'est quoi ce site</title>
+    <link rel="stylesheet" href="./style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <!-- NAVBAR -->
+        <div id="head">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Staff</a></li>
+                <li><a href="#">Règles</a></li>
+                <li><a href="#">Aide et support</a></li>
+                <li><a href="#" id="signin">SIGN IN</a></li>
+                <li><a href="#" id="signup">SIGN UP</a></li>
+            </ul>
+        </div>
+    </header>
+    <main>
+        <!-- SECTION -->
+        <div class="container">
+            <!-- LOGO -->
+            <div id="logo"><img src="./bg.png" alt="logo"></div>
+            <!-- RESUMÉ -->
+            <div id="resume">
+                <h1 id="titre">Blood Moon</h1><br /><br />
+                <p>Blood Moon est un jeux vidéo en vue isométrique
+                    qui reprend les bases
+                    du jeux de société Loup Garou de Thiercelieux.
+                </p><br /><br /><br />
+                <a id="more" href="#">READ MORE <i class="fa-solid fa-right-long"></i></a>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="foot">
+            <h1>Download for free</h1>
+            <button id="download">Download</button>
+        </div>
+    </footer>
+</body>
+
+</html>
